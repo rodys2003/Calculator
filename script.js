73097e97d7df57d3e3d84b5fd399a56cbe8ea7c1
rodis2003 <rodisenrique73@gmail.com>
2023-06-27 17:10:12 -0500

    The error in the operations between operators was found and fixed

--- script.js
+++ script.js
@@ -10,15 +10,28 @@ buttons.forEach((item)=>{
             let string = display.innerText.toString();
             display.innerText = string.substr(0,string.length-1);
         }
-        else if(display.innerText != "" && item.id=="equal"){
-            display.innerText = eval(display.innerText);
-        }
-        else if(display.innerText == "" && item.id == "equal"){
-            display.innerText ="Null";
-            setTimeout(() => (display.innerText = ""),2000);
+        else if(item.id == "equal"){
+            if(display.innerText != ""){
+                if(validDate(display.innerText)){
+                    display.innerText = eval(display.innerText);
+                }
+                else{
+                    display.innerText ="Null";
+                    setTimeout(() => (display.innerText = ""),2000);
+                }
+            }
+            else{
+                display.innerText ="Null";
+                setTimeout(() => (display.innerText = ""),2000);
+            }
         }
         else{
             display.innerText += item.id;
         }
     };
 });
+
+function validDate (date){
+    let pattern = /[\+\-\/\*]{2,}/;
+    return !pattern.test(date);
+}
